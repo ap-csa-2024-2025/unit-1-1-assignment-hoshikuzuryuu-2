8a83995f7e16edb88cc99941934f26c8b995c63c
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-09-04 09:54:20 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAm done...

--- problem1.java
+++ problem1.java
@@ -1,1 +1,11 @@
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        
+        String author;
+        author = "Fukuda Shinichi";
+        System.out.print("My favorite author is the manga author "+ author +", the author of My Dress Up Darling!");
 
+    }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,13 @@
+public class problem3
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("=========");
+        System.out.println("= 0   0 =   Java is Awesome!");
+        System.out.println("=   v   =");
+        System.out.println("=========");
+    }
+}
+
 
